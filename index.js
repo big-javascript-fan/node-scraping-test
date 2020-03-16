e72e4b33fd19f5180e493503307bcf8f7ddad5cd
javascriptFan <wdeveloping9099@gmail.com>
2020-03-16 23:18:15 +0800

    getting visible contents

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let mainURL = 'https://www.disneyplus.com/login';   //first url to disneyplus
 const main = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 926 });
+  // await page.setViewport({ width: 1920, height: 926 });
   await page.goto(mainURL);
   await page.waitFor(8000);
 
@@ -24,48 +24,57 @@ const main = async () => {
   await page.waitForSelector("#email");
 
   const emailInputField = await page.$('#email');
-  emailInputField.value = "dtest-1984@gmail.com";
+  emailInputField.click();
+  await emailInputField.type("dtest-1984@gmail.com");
 
-  const continueButton = await page.$('button[type="submit"]');
+  const continueButton = await page.$('button[value="submit"]');
+  
   continueButton.click();
 
+  await page.waitFor(3000);
   await page.waitForSelector('#password');
 
   const passwordInputField = await page.$('#password');
-  passwordInputField.value = 'Test!234';
+  passwordInputField.click();
+  await passwordInputField.type('Test!234');
 
-  const loginButton = await page.$('button[type="submit"]');
+  const loginButton = await page.$('button[value="submit"]');
   loginButton.click();
 
+  await page.waitFor(4000);
   await page.waitForSelector('#home-collection');
 
-  let movieSections = await page.$('#home-collection > div');
-  console.log(movieSections.length);
+  loginButton.asElement().
+  // get movie details
+  let movieData = await page.evaluate(() => {
+      let movies = [];
+      // get the movie elements
+      let moviesElms = document.querySelectorAll('#home-collection > div');
+      // get the movie data
+      moviesElms.forEach((sectionElement) => {
+          let sectionJson = {};
+          try {
+              sectionJson.name = sectionElement.querySelector('h4').innerText;
+              sectionJson.items = {};
+              let sliders = sectionElement.querySelectorAll('.slick-slide');
+              sliders.forEach(slider => {
 
-  // // get movie details
-  // let movieData = await page.evaluate(() => {
-  //     let movies = [];
-  //     // get the movie elements
-  //     let moviesElms = document.querySelectorAll('div.sr_property_block[data-movieid]');
-  //     // get the movie data
-  //     moviesElms.forEach((sectionElement) => {
-  //         let sectionJson = {};
-  //         try {
-  //             sectionJson.name = sectionElement.querySelector('span.sr-section__name').innerText;
-  //             sectionJson.reviews = sectionElement.querySelector('span.review-score-widget__subtext').innerText;
-  //             sectionJson.rating = sectionElement.querySelector('span.review-score-badge').innerText;
-  //             if(sectionElement.querySelector('strong.price')){
-  //                 sectionJson.price = sectionElement.querySelector('strong.price').innerText;
-  //             }
-  //         }
-  //         catch (exception){
+                console.log(slider);
+                let item = {};
+                item.name = slider.querySelector('.image-container').getAttribute('alt');
+                item.image = slider.querySelector('img').getAttribute('src');
+                // window.history.back();
+              });
+          }
+          catch (exception){
+            console.log('error happened on dom translation');
+          }
+          movies.push(sectionJson);
+      });
+      return movies;
+  });
 
-  //         }
-  //         movies.push(sectionJson);
-  //     });
-  //     return movies;
-  // });
-
+  console.log(movieData);
   // fs.writeFile("result.json", JSON.stringify(movieData), function(err) {
   //   if (err) throw err;
   //   console.log("Saved!");
